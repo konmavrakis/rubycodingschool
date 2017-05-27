6cba1c5cc37c672fc1151ada085728002769b4ce
epi <epi4@hotmail.com>
2017-05-27 17:23:05 +0300

    first database schema creation

---
+++ app/views/lists/_list.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! list, :id, :name, :active, :total_price, :created_at, :updated_at
+json.url list_url(list, format: :json)
---
+++ db/migrate/20170527142046_create_product_lists.rb
@@ -0,0 +1,10 @@
+class CreateProductLists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :product_lists do |t|
+      t.integer :list_id
+      t.integer :product_id
+
+      t.timestamps
+    end
+  end
+end
