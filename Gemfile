153c1f66c7acf4dcd2269f1259f6f9e730b8c028
JMiliaresis <jmiliaresis@hostwork.net>
2017-05-29 22:10:40 +0300

    refactoring user model, added twitter bootstrap static, having fun with template

--- Gemfile
+++ Gemfile
@@ -14,9 +14,6 @@ gem 'mysql2', '>= 0.3.18', '< 0.5'
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
 
-gem 'bootstrap-sass', '~> 3.3.6'
-
-
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
@@ -24,6 +21,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 gem 'therubyracer', platforms: :ruby
+
+gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
+gem "twitter-bootstrap-rails"
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,29 @@
 class User < ApplicationRecord
+  # Associations
+  # ===========
+  
+  has_many :lists
+ 
+
+  # Devise Config
+  # =============
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # #3 password must be at least 8 chars and not bigger than 16.
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 8..16
 
-  has_many :lists
+
+  # Validations
+  # ===========
+
+  # name and email should be unique
   validates :email, uniqueness: true
-  validates :name, uniqueness: { scope: :email,  message: 'there can be only one!' }
+  # name should be bigger than 4 chars
+  validates :name, length: { minimum: 4, maximum: 500, wrong_length: 'Invalid length', too_long: "%{count} characters is the maximum allowed", too_short: "must have at least %{count} characters"  }
 
-  devise :database_authenticatable, :validatable, password_length: 8..16
+  # name and email should be unique
+  validates :name, uniqueness: { scope: :email,  message: 'there can be only one!'} 
   
 end
