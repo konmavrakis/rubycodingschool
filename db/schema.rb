1ebda05d38dc9dd8abbdcc37ffd0c1f210c4c6bf
epi <epi4@hotmail.com>
2017-06-03 16:36:11 +0300

    update schema after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527142046) do
+ActiveRecord::Schema.define(version: 20170602210147) do
 
   create_table "lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -18,13 +18,19 @@ ActiveRecord::Schema.define(version: 20170527142046) do
     t.decimal  "total_price", precision: 10
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "users_id"
+    t.index ["users_id"], name: "index_lists_on_users_id", using: :btree
   end
 
   create_table "product_lists", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "list_id"
     t.integer  "product_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "products_id"
+    t.integer  "lists_id"
+    t.index ["lists_id"], name: "index_product_lists_on_lists_id", using: :btree
+    t.index ["products_id"], name: "index_product_lists_on_products_id", using: :btree
   end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -37,11 +43,24 @@ ActiveRecord::Schema.define(version: 20170527142046) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "username"
-    t.string   "password"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "lists", "users", column: "users_id"
+  add_foreign_key "product_lists", "lists", column: "lists_id"
+  add_foreign_key "product_lists", "products", column: "products_id"
 end
