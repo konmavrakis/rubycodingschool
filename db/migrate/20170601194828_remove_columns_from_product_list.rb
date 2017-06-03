2473df22273dd69b9d692082947511c3dfcf1727
epi <epi4@hotmail.com>
2017-06-03 23:10:03 +0300

    Updates schema and corrects column removal migration file

--- db/migrate/20170601194828_remove_columns_from_product_list.rb
+++ db/migrate/20170601194828_remove_columns_from_product_list.rb
@@ -1,6 +1,6 @@
 class RemoveColumnsFromProductList < ActiveRecord::Migration[5.0]
-  def change_table:product_lists
-    remove_column :product_lists, :list_id
-    remove_column :product_lists, :product_id
+  def change
+    remove_column :product_lists, :list_id, :integer
+    remove_column :product_lists, :product_id, :integer
   end
 end
