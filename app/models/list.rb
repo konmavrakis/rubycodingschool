32704530951f9d0273cf6911ff12ad9ee9043b7a
epi <epi4@hotmail.com>
2017-06-03 15:59:18 +0300

    update models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
-  belongs_to:users
-  has_many:product_lists
-  has_many:products, through :product_lists
+  belongs_to :users
+  has_many :product_lists
+  has_many :products, through :product_lists
 end
