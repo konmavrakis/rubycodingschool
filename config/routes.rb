a0c7624e66935c767683da0f4ffb6a5218179d61
Ntinos Mavrakis <konmavrakis@gmail.com>
2017-06-17 11:38:20 +0300

    Fix missing ' in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'pages#home'
-  get 'about', to: 'pages#about
+  get 'about', to: 'pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
