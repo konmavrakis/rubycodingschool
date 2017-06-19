3895202b9598fb705b774256cbaf42e90eb93e6e
vsiopis <vsiopi@gmail.com>
2017-06-19 22:40:56 +0300

    optimaze code

--- config/routes.rb
+++ config/routes.rb
@@ -18,8 +18,8 @@ Rails.application.routes.draw do
 
   resources :product_lists, except: [:index, :new, :show]
   post 'product_lists/create', to: 'product_lists#create'
-  get 'product_lists/increase_quantity', to: "product_lists#increase_quantity"
-  get 'product_lists/decrease_quantity', to: "product_lists#decrease_quantity"
+  post 'product_lists/increase_quantity', to: "product_lists#increase_quantity"
+  post 'product_lists/decrease_quantity', to: "product_lists#decrease_quantity"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
