6c97a4be9b586d98b715954f73c38b5489c1420a
epi <epi4@hotmail.com>
2017-06-24 10:43:38 +0300

    Implement pagination for home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-  	@list = List.where('active=?', 1).order('created_at desc')
+  	@list = List.where('active=?', 1).order('created_at desc').page(params[:page]).per(9)
   end
 end
