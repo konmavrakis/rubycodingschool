c5dbd311f212b248fc0a4fdf84a032ae02ba835a
JMiliaresis <jmiliaresis@hostwork.net>
2017-06-28 20:24:37 +0300

    searchable list init commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-  	@list = List.where('active=?', true).order('created_at desc').page(params[:page]).per(9)
+    @list = List.search(params[:keyword],1, params[:page] )
   end
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,14 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :product_lists, dependent: :destroy
   has_many :products, through: :product_lists
+
+  def self.search(search, active = 1, page = 1, pages = 9 )
+    wildcard_search = "%#{search}%"
+
+    where("active = :active AND name LIKE :search ", active: active,  search: wildcard_search)
+      .page(page)
+      .per(pages)
+      .order('created_at desc')
+  end
+
 end
