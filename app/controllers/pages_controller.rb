5534887832b1134fe44151f457e1177442a8f250
epi <epi4@hotmail.com>
2017-07-01 11:52:07 +0300

    Add comments in About page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
+
   def home
   	@list = List.where('active=?', true).order('created_at desc').page(params[:page]).per(9)
   end
+
+  def about
+    @REVISION = `git log --pretty=format:'%h' -n 1`
+  end
+
 end
