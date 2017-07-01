68464ab22b5b4d491dba5aea1dd484680c980482
Ntinos Mavrakis <konmavrakis@gmail.com>
2017-07-01 21:03:13 +0300

    Add alerts for new product, product quantity increase and product delete

--- app/controllers/product_lists_controller.rb
+++ app/controllers/product_lists_controller.rb
@@ -1,21 +1,29 @@
 class ProductListsController < ApplicationController
+
+  include SkroutzApi
+
   def create
     sku_id = params[:sku_id]
     list = List.find(params[:list])
-    quantity = params[:quantity].to_i
+    quantity = params[:quantity]
     product_list = ProductList.find_by(list_id: list.id, sku_id: sku_id)
     if product_list 
-      product_list.quantity = product_list.quantity + quantity
-      product_list.save! 
+      product_list.quantity = product_list.quantity + quantity.to_i
+      product_list.save!
+      redirect_to( list , notice: 'You just increased the quantity of ' + SkroutzApi.find_sku(sku_id).name + ' by ' + quantity.to_s + '.')
     else 
-      ProductList.create(list_id: list.id, sku_id: sku_id, quantity: quantity) 
-    end
-    redirect_to list_path list
+      ProductList.create(list_id: list.id, sku_id: sku_id, quantity: quantity)
+      redirect_to( list , notice: 'You just added ' + SkroutzApi.find_sku(sku_id).name  + '  to ' + list.name + ' list!'  ) 
+    end    
   end
 
   def destroy
-		ProductList.find_by(sku_id: params[:sku_id], list_id: params[:list_id]).delete
-		redirect_to :back 
+    list = List.find(params[:list_id])
+    sku_id = params[:sku_id]
+    
+    ProductList.find_by(sku_id: sku_id, list_id: list.id).delete
+    
+    redirect_to( list , notice: 'You just removed ' + SkroutzApi.find_sku(sku_id).name  + '  from ' + list.name + ' list!'  ) 
   end
 
 end
