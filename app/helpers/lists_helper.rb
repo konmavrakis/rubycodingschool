bf0dc627e7356dbb3e47a7e023ecfda580d8f81b
Ntinos Mavrakis <konmavrakis@gmail.com>
2017-07-01 14:41:31 +0300

    Fix total price in list

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,16 @@
 module ListsHelper
+
+	def total_price(quantity, price)
+		quantity * price
+	end
+
+	def list_total_price(skus)
+		total_price = 0
+		skus.each do |s|
+			price = s[:sku].price_min * s[:quantity]
+			total_price += price
+		end
+		total_price
+	end
+
 end
