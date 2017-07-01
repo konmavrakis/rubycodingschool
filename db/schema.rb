93e2a73357149a926eb9088f907f14a9e9f0ae4a
vsiopis <vsiopis@gmail.com>
2017-07-01 17:34:01 +0300

    Create seeds for FavoriteList and ProductList

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630175702) do
+ActiveRecord::Schema.define(version: 20170701092222) do
 
   create_table "average_caches", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "rater_id"
