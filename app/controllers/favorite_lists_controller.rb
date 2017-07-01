4cc21b55c640d05ad2243080c83e14246c80eccf
TsiroJohn <thanos.tsiro@gmail.com>
2017-07-01 14:30:50 +0300

    favorite_list functionalities

--- app/controllers/favorite_lists_controller.rb
+++ app/controllers/favorite_lists_controller.rb
@@ -1,17 +1,25 @@
 class FavoriteListsController < ApplicationController
 
     def index
-      @lists = FavoriteList.where(user_id: current_user.id)
-      
+      @fav_lists = FavoriteList.joins(:list).includes(:list).where(user_id: current_user.id)
+      # @list = List.where('active=?', true).order('created_at desc').page(params[:page]).per(9)
     end 
 
     def create
-        FavoriteList.create(list_id: params[:list_id],user_id: current_user.id)
-        redirect_to :back
+        @fav_list = FavoriteList.create(list_id: params[:list_id],user_id: current_user.id)
+
+        respond_to do |format|   
+            format.html { redirect_to :back, notice: 'List was added to favorites' }
+            format.json { render json: {resp: 'ok', list: @fav_list} }           
+        end
+
     end    
 
-    def destroy
+    def destroy 
         FavoriteList.find(params[:id]).delete
-        redirect_to :back
+        respond_to do |format|   
+            format.html { redirect_to :back, notice: 'List was removed from favorites' }
+            format.json { render json: {resp: 'ok'} }          
+        end
     end
 end
